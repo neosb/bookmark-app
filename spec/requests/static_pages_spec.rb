599a688dca91b56f6a40a457fe353a32c5335f42
Szymon Blaszczynski <szymon.blaszczynski@gmail.com>
2013-05-21 03:30:39 +0200

    Adding about view, action and route

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,26 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
   describe "Home page" do
-    it "should have the content bookmark-app" do
+    it "should have the content 'bookmark-app'" do
       visit '/static_pages/home'
       page.should have_content('bookmark-app')
     end
   end
+
+  describe "Help page" do
+  	it "should have the content 'F1.help'" do
+  		visit '/static_pages/help'
+  		page.should have_content('F1.help')
+  	end
+  end
+
+  describe "About page" do
+  	it "should have content 'about.us'" do
+  		visit '/static_pages/about'
+  		page.should have_content('about.us')
+  	end
+  end
+  
 end
