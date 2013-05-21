6b7df9aaebe9bb8095cd144b58d82a76ca89c6a8
Szymon Blaszczynski <szymon.blaszczynski@gmail.com>
2013-05-21 12:31:41 +0200

    added proper routing, but somehow have_content spec
    fails on home

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,10 +5,10 @@ describe "StaticPages" do
 	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
-    it "should have the content 'bookmark-app'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'bookmark-app')
-    end
+    # it "should have 'host.bookmarks'" do
+    #   visit root_path
+    #   page.should have_content('host.bookmarks')
+    # end
    #  it "should have title '@.home'" do
   	# 	visit '/static_pages/about'
   	# 	page.should have_selector('title', :text => '@.home')
@@ -16,9 +16,9 @@ describe "StaticPages" do
   end
 
   describe "Help page" do
-  	it "should have the content 'F1.help'" do
-  		visit '/static_pages/help'
-  		page.should have_selector('h1', :text => 'F1.help')
+  	it "should have h1 'F1.help'" do
+  		visit help_path
+  		page.should have_selector('h1', text: 'F1.help')
   	end
   	# it "should have title 'F1.help'" do
   	# 	visit '/static_pages/about'
@@ -27,9 +27,9 @@ describe "StaticPages" do
   end
 
   describe "About page" do
-  	it "should have content 'about.us'" do
-  		visit '/static_pages/about'
-  		page.should have_selector('h1', :text => 'about.us')
+  	it "should have h1 'about.us'" do
+  		visit about_path
+  		page.should have_selector('h1', text: 'about.us')
   	end
   	# it "should have title 'about.us'" do
   	# 	visit '/static_pages/about'
@@ -39,7 +39,7 @@ describe "StaticPages" do
 
   describe "Contact page" do
   	it "should have content 'szymon.blaszczynski@gmail.com'" do
-  		visit '/static_pages/contact'
+  		visit contact_path
   		page.should have_content('szymon.blaszczynski@gmail.com')
   	end
   end
