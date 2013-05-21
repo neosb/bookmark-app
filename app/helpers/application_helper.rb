01d60cebb450acc85ef0c316de4e7cf6f334a3a8
Szymon Blaszczynski <szymon.blaszczynski@gmail.com>
2013-05-21 08:21:21 +0200

    title rendered from application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# return title based on input string
+	def full_title page_title
+		base_title = "bookmark-app"
+		if page_title.empty?
+			base_title
+		else
+			"#{base_title} | #{page_title}"
+		end
+	end
 end
